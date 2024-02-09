aa3c39d7c815526c2ba9936dd6957d3a49c4b538
voidcht <chathurangilansakara@gmail.com>
2024-02-09 16:26:54 +0530

    todolist version two before finishing

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,26 @@
-const todoList = [];
+//const todoList = [];
+
+//For the second version of the app, let's have some default values for the array
+const todoList = [
+  "make dinner",
+  "watch a film",
+  "write journal"
+];
+
+let todoListHTML = '';
+
+for(let i =0; i< todoList.length; i++){
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`;
+  //accumulator string - to combine all the <p> items for to do list items
+  todoListHTML += html;
+}
+//display accumulator result
+console.log(todoListHTML);
+
+
+document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
 
 function addTodo(){
   const inputElement = document.querySelector('.js-todo-item');
