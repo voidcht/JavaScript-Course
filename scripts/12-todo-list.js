7e01e9de5c076192c4b4df2b5a7eb14a8062b520
voidcht <chathurangilansakara@gmail.com>
2024-03-13 19:33:18 +0530

    using forEach() in todo list app instead of for loop

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -16,30 +16,49 @@ function renderToDoList(){
   //a string to accumulate the result
   let todoListHTML = '';
 
+//using forEach() method to loop through the array
+//instead of for loop (as below)
+  todoList.forEach(function(todoObject,index){
+    //same code as used in the for-loop
+    //todoList[i] = todoObject now and i  = index
+    
+    const{name, dueDate} = todoObject;
 
-  
-
-  //for loop is used to loop through the array
-  for(let i =0; i< todoList.length; i++){
-    //updating to do list item as an object instead of a string
-    const todoObject = todoList[i];
-    //const name = todoObject.name;
-  //object restructuring can be used instead of above line
-    const {name, dueDate} = todoObject; 
-    //const dueDate = todoObject.dueDate;
     const html = `
-      <div>${name}</div>
-      <div>${dueDate}</div>
-      <button class="delete-todo-button" onClick="
-        todoList.splice(${i},1);
-        renderToDoList();
-      "> Delete </button> 
-    `;
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button class="delete-todo-button" onClick="
+      todoList.splice(${index},1);
+      renderToDoList();
+    "> Delete </button> 
+  `;
 
-    //accumulator string - to combine all the <p> items for to do list items
-    todoListHTML += html;
-  }
-  //end of for loop
+  todoListHTML += html;
+
+  });
+
+
+  // //for loop is used to loop through the array
+  // for(let i =0; i< todoList.length; i++){
+  //   //updating to do list item as an object instead of a string
+  //   const todoObject = todoList[i];
+  //   //const name = todoObject.name;
+  // //object restructuring can be used instead of above line
+  //   const {name, dueDate} = todoObject; 
+  //   //const dueDate = todoObject.dueDate;
+  //   const html = `
+  //     <div>${name}</div>
+  //     <div>${dueDate}</div>
+  //     <button class="delete-todo-button" onClick="
+  //       todoList.splice(${i},1);
+  //       renderToDoList();
+  //     "> Delete </button> 
+  //   `;
+
+  //   //accumulator string - to combine all the <p> items for to do list items
+  //   todoListHTML += html;
+  // }
+  // //end of for loop
 
 
 
